6685fe1a09e3a56508a3c1fb8fcc38ecacf12d1a
stevegotham <g23design@gmail.com>
2016-05-11 21:59:05 -0600

    a couple solutions to problem set 4

--- problem-set-4/main.js
+++ problem-set-4/main.js
@@ -15,3 +15,57 @@ function addNumbers (str) {
   }
   return sum;
 }
+
+function longestWord (str) {
+  var words = str.split(' ');
+  var longWord = '';
+  var letterCount = 0;
+  for (var i=0; i<words.length; i++) {
+    words[i] = {
+      word: words[i],
+      letters: words[i].length
+    }
+    for (var ii=0;ii<words[i].word.length; ii++) {
+      if (words[i].word[ii] === '.' ||
+          words[i].word[ii] === ',' ||
+          words[i].word[ii] === '?' ||
+          words[i].word[ii] === '!' ||
+          words[i].word[ii] === "'" ||
+          words[i].word[ii] === "-") {
+          words[i].letters -= 1
+      }
+    }
+    if (words[i].letters > letterCount) {
+      letterCount = words[i].letters
+      longWord = words[i].word
+    }
+  }
+  return longWord;
+}
+
+function longestWord2 (str) {
+  var words = str.split(' ');
+  var longWord = '';
+  var letterCount = 0;
+  for (var i=0; i<words.length; i++) {
+    words[i] = {
+      word: words[i],
+      letters: words[i].length
+    }
+  }
+  for (var word in words) {
+    if (words[word].word.indexOf('.') >= 0 ||
+        words[word].word.indexOf(',') >= 0 ||
+        words[word].word.indexOf('!') >= 0 ||
+        words[word].word.indexOf('?') >= 0 ||
+        words[word].word.indexOf('-') >= 0 ||
+        words[word].word.indexOf("'") >= 0) {
+        words[word].letters -= 1;
+    }
+    if (words[word].letters > letterCount) {
+      letterCount = words[word].letters
+      longWord = words[word].word
+    }
+  }
+  return longWord
+}
